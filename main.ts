351a2c94d62723f56cb6303aadb914b44a02d3cb
Victor Guilherme <92493696+Victor0814gui@users.noreply.github.com>
2023-05-07 11:22:38 -0300

    refactor enviroments

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 // Defina o caminho do arquivo CSV que você deseja ler
 const filePath = "database.csv";
 
-
+const columnCsv = 13;
 let lostTrades = 0;
 // Crie uma função para ler o arquivo CSV e retornar a sexta coluna
 function readCSVAndGetSixthColumn(filePath: string): string[] {
@@ -16,11 +16,11 @@ function readCSVAndGetSixthColumn(filePath: string): string[] {
   // Mapeie as linhas para obter a sexta coluna
   const sixthColumnValues = lines.map((line) => {
     const columns = line.split(',');
-    if (columns[13] === "0") {
+    if (columns[columnCsv] === "0") {
       lostTrades = lostTrades + 1;
     }
     console.log(lostTrades)
-    return columns[13]; // a sexta coluna tem o índice 5 (os índices começam em 0)
+    return columns[columnCsv]; // a sexta coluna tem o índice 5 (os índices começam em 0)
   });
 
   // Retorne um array com os valores da sexta coluna
